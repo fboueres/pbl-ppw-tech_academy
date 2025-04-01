d10f165cb33a2a2acb3ee80e58b6c6385737ab82
Fernando Boueres <boueres.fernando@gmail.com>
2025-04-01 21:22:01 +0000

    Adapting Vite environment for remote development

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        server: {
+            host: env.VITE_HOST || 'localhost',
+            port: env.VITE_PORT || 5173,
+            hmr: {
+                host: env.VITE_HMR_HOST,
+            },
+            cors: {
+                origin: env.VITE_CORS_ORIGIN
+            },
+        },
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            tailwindcss(),
+        ],
+    };
 });
